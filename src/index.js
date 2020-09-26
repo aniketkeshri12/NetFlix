8d0ab3d5ba83821196efc644de33c91a92dea673
Aniket keshri <67973829+aniketkeshri12@users.noreply.github.com>
2020-09-26 17:20:15 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,48 +4,21 @@ import Card from "./Card.jsx";
 import "./styles.css";
 import data from "./Sdata.jsx";
 
+function ncard(val) {
+  return (
+    <Card
+      imgsrc={val.imgsrc}
+      title={val.title}
+      sname={val.sname}
+      link={val.link}
+    />
+  );
+}
+
 ReactDOM.render(
   <>
     <h1 className="heading_style">The Best 6 Shows of Netflix</h1>
-    <Card
-      imgsrc={data[0].imgsrc}
-      title={data[0].title}
-      sname={data[0].sname}
-      link={data[0].link}
-    />
-
-    <Card
-      imgsrc={data[1].imgsrc}
-      title={data[1].title}
-      sname={data[1].sname}
-      link={data[1].link}
-    />
-
-    <Card
-      imgsrc={data[2].imgsrc}
-      title={data[2].title}
-      sname={data[2].sname}
-      link={data[2].link}
-    />
-
-    <Card
-      imgsrc={data[3].imgsrc}
-      title={data[3].title}
-      sname={data[3].sname}
-      link={data[3].link}
-    />
-    <Card
-      imgsrc={data[4].imgsrc}
-      title={data[4].title}
-      sname={data[4].sname}
-      link={data[4].link}
-    />
-    <Card
-      imgsrc={data[5].imgsrc}
-      title={data[5].title}
-      sname={data[5].sname}
-      link={data[5].link}
-    />
+    {data.map(ncard)};
   </>,
   document.getElementById("root")
 );
