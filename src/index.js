8edc7b725f6efe9649b4ce8a4eb2af17e53c1ae8
Aniket keshri <67973829+aniketkeshri12@users.noreply.github.com>
2020-09-26 17:36:04 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -4,21 +4,21 @@ import Card from "./Card.jsx";
 import "./styles.css";
 import data from "./Sdata.jsx";
 
-function ncard(val) {
-  return (
-    <Card
-      imgsrc={val.imgsrc}
-      title={val.title}
-      sname={val.sname}
-      link={val.link}
-    />
-  );
-}
-
 ReactDOM.render(
   <>
     <h1 className="heading_style">The Best 6 Shows of Netflix</h1>
-    {data.map(ncard)};
+    {data.map((val) => {
+      return (
+        <Card
+          imgsrc={val.imgsrc}
+          title={val.title}
+          sname={val.sname}
+          link={val.link}
+        />
+      );
+    })}
+    ;
   </>,
+
   document.getElementById("root")
 );
